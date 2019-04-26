9c705a423956f5574ab9f57ffb892c8454994ba1
Dev <dev90@live.in>
2019-04-26 23:53:37 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+
+  onDiscover(){
+    try {
+
+    let tokenservice=null;
+
+    navigator.bluetooth.requestDevice({
+      acceptAllDevices:true
+    }).then(device => device.gatt.connect())
+    .then(server => server.getPrimaryService('heart_rate'))
+    .then(service => {tokenservice = service;
+      return Promise.all([
+        service.getCharacteristic('body_sensor_location')
+          .then(handleTokenCharacteristic)
+      ]);
+    });
+
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div><h1>BlueTooth Device</h1></div>
+        <div>
+          <input type='button' value='Discover Device' onClick={this.onDiscover}></input>
+        </div>
     </div>
   );
+}}
+
+
+function handleTokenCharacteristic(characteristic){
+  if (characteristic === null) {
+    console.log("Unknown sensor location.");
+    return Promise.resolve();
+  }
+
+  console.log(characteristic);
+  return Promise.resolve();
 }
 
 export default App;
---
+++ src/device.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Device() {
+  return (
+    <div>
+     <div>hello</div>
+    </div>
+  );
+}
+
+export default Device;
